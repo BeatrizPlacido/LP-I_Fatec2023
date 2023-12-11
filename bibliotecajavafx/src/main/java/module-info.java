42bec7e58e60fc99a62acfb8d66e4e2dfb7d40ee
BeatrizPlacido <127264395+BeatrizPlacido@users.noreply.github.com>
2023-12-11 18:40:40 -0300

    Revert ":white_check_mark: Incluindo atividade de classes"
    This reverts commit 3380c3ff0512d3be0c9101465751835ee8c64823.

--- bibliotecajavafx/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.bibliotecajavafx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.bibliotecajavafx to javafx.fxml;
-    exports com.example.bibliotecajavafx;
-}
